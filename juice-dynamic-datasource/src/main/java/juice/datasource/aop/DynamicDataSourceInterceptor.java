261261e921c932d314b2d72ce20b9039da44f12b
冯兵兵 <fengbingbing@iqianjin.com>
2021-04-28 10:59:48 +0800

    优化动态数据源切面逻辑

--- juice-dynamic-datasource/src/main/java/juice/datasource/aop/DynamicDataSourceInterceptor.java
+++ juice-dynamic-datasource/src/main/java/juice/datasource/aop/DynamicDataSourceInterceptor.java
@@ -31,6 +31,9 @@ public class DynamicDataSourceInterceptor implements MethodInterceptor {
         Class<?> targetClass = method.getDeclaringClass();
 
         String pkgName = targetClass.getPackage().getName();
+        String className = ClassUtils.getShortName(targetClass);
+        String methodName = method.getName();
+
         //优先从方法上查找
         DSRouting dsRouting = AnnotationUtils.getAnnotation(method, DSRouting.class);
         if (dsRouting == null) {    //尝试从类上查找
@@ -39,8 +42,7 @@ public class DynamicDataSourceInterceptor implements MethodInterceptor {
 
         // 没有这个注解说明不正常，直接抛出异常
         if (dsRouting == null) {
-            LOG.warn("【动态数据源切换】没有数据源切换标识, 不做任何处理, method={}, class={}",
-                    method.getName(), ClassUtils.getShortName(targetClass));
+            LOG.warn("【动态数据源切换】没有数据源切换标识, 不做任何处理, method={}, class={}", methodName, className);
             return invocation.proceed();
         }
 
@@ -50,13 +52,13 @@ public class DynamicDataSourceInterceptor implements MethodInterceptor {
             selectDsKey = this.dynamicDataSource.getPkgDefaultDsKey(pkgName);
             if (LOG.isDebugEnabled()) {
                 LOG.debug("【动态数据源切换】注解上配置为空, 尝试根据包名来获取, pkgName:{}, method={}, class={}, selectDsKey:{}",
-                        pkgName, method.getName(), ClassUtils.getShortName(targetClass), selectDsKey);
+                        pkgName, methodName, className, selectDsKey);
             }
         }
 
         if (StringUtils.isEmpty(selectDsKey)) {
             LOG.error("【动态数据源切换】没有找到数据源标识key 请检查代码配置, method={}, class={}",
-                    method.getName(), ClassUtils.getShortName(targetClass));
+                    methodName, className);
             throw new IllegalArgumentException("没有找到数据源标识key 请检查代码配置");
         }
 
@@ -64,8 +66,7 @@ public class DynamicDataSourceInterceptor implements MethodInterceptor {
         String beforeDsKey = this.dynamicDataSource.getDataSourceKey();
 
         if (beforeDsKey != null && beforeDsKey.equals(selectDsKey)) {   //数据源相同，直接执行即可
-            LOG.warn("【动态数据源切换】-数据源相同, 直接执行即可, method={}, class={}",
-                    method.getName(), ClassUtils.getShortName(targetClass));
+            LOG.warn("【动态数据源切换】-数据源相同, 直接执行即可, method={}, class={}", methodName, className);
             return invocation.proceed();
         }
 
@@ -73,19 +74,26 @@ public class DynamicDataSourceInterceptor implements MethodInterceptor {
         this.dynamicDataSource.setDataSourceKey(selectDsKey);
 
         if (LOG.isDebugEnabled()) {
-            LOG.debug("【动态数据源切换】路由主键配置:{}, 数据源从={} 切换到={})",
-                    dsRouting.value(), beforeDsKey, selectDsKey);
+            LOG.debug("【动态数据源切换】路由主键配置:{}, 数据源从={} 切换到={}, method={}, class={}",
+                    dsRouting.value(), beforeDsKey, selectDsKey, methodName, className);
         }
 
         try {
             return invocation.proceed();
         } finally {
-            if (LOG.isDebugEnabled()) {
-                LOG.debug("【动态数据源切换】方法执行完成 恢复现场，当前数据源={}, 历史数据源={}", selectDsKey, beforeDsKey);
-            }
             if (beforeDsKey != null) {
+                if (LOG.isDebugEnabled()) {
+                    LOG.debug("【动态数据源切换】方法执行完成-恢复数据源，当前数据源={}, 历史数据源={}, method={}, class={}",
+                            selectDsKey, beforeDsKey, methodName, className);
+                }
                 //执行完成后恢复
                 this.dynamicDataSource.setDataSourceKey(beforeDsKey);
+            } else {
+                if (LOG.isDebugEnabled()) {
+                    LOG.debug("【动态数据源切换】方法执行完成-清理数据源，当前数据源={}, 历史数据源={}, method={}, class={}",
+                            selectDsKey, beforeDsKey, methodName, className);
+                }
+                this.dynamicDataSource.clear();
             }
         }
     }
